204ffeb712c842ba7ced3df602451edd64a65588
Felipe Lima <lipezera1836@gmail.com>
2024-07-24 10:46:15 -0300

    docs: english documentation

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import c from "config";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json";
+import swaggerDocumentEn from "./swagger.json";
+import swaggerDocumentBr from "./swagger-pt-br.json";
 import { connection } from "../config/db";
 import { UserRoutes } from "./routes/User.routes";
 import { ArenaRoutes } from "./routes/Arena.routes";
 const app = express();
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  "/api-docs/en",
+  swaggerUi.serveFiles(swaggerDocumentEn),
+  swaggerUi.setup(swaggerDocumentEn)
+);
+app.use(
+  "/api-docs/br",
+  swaggerUi.serveFiles(swaggerDocumentBr),
+  swaggerUi.setup(swaggerDocumentBr)
+);
 
 app.use("/user/", UserRoutes);
 app.use("/arena/", ArenaRoutes);
