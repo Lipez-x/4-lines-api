64b4b49e3c257f3ff889c32ba795775f60069928
Felipe Lima <lipezera1836@gmail.com>
2024-07-26 14:43:27 -0300

    feat(user): protect routes by user role

--- src/routes/Arena.routes.ts
+++ src/routes/Arena.routes.ts
@@ -11,8 +11,19 @@ const arenaController = new ArenaController();
 
 ArenaRoutes.post("/create", verifyToken, isOwner, arenaController.create);
 ArenaRoutes.get("/", verifyToken, arenaController.getAll);
-ArenaRoutes.get("/myarenas", verifyToken, arenaController.getAllUserArenas);
-ArenaRoutes.get("/:id", verifyToken, isValidId, arenaController.getById);
+ArenaRoutes.get(
+  "/myarenas",
+  verifyToken,
+  isOwner,
+  arenaController.getAllUserArenas
+);
+ArenaRoutes.get(
+  "/:id",
+  verifyToken,
+  isValidId,
+  isOwner,
+  arenaController.getById
+);
 ArenaRoutes.put(
   "/:id/:hourId",
   verifyToken,
@@ -24,18 +35,27 @@ ArenaRoutes.put(
   "/accept/:id/:hourId/:lesseeId",
   verifyToken,
   isValidId,
+  isOwner,
   arenaController.acceptRequest
 );
 ArenaRoutes.put(
   "/complete/:id/:hourId",
   verifyToken,
   isValidId,
+  isOwner,
   arenaController.completeRental
 );
-ArenaRoutes.put("/update/:id", verifyToken, isValidId, arenaController.update);
+ArenaRoutes.put(
+  "/update/:id",
+  verifyToken,
+  isValidId,
+  isOwner,
+  arenaController.update
+);
 ArenaRoutes.delete(
   "/delete/:id",
   verifyToken,
   isValidId,
+  isOwner,
   arenaController.delete
 );
